62c08899ae123a1b9277f9b8e5ec816beb404467
maddiepero <60265399+maddiepero@users.noreply.github.com>
2022-07-06 13:03:56 -0400

    Update setup.py

--- example_package/setup.py
+++ example_package/setup.py
@@ -19,7 +19,7 @@ setup(
     license='BSD-3',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': ['test_executable_2 = example_package.bump_color_rotate:main',
+        'console_scripts': ['test_executable_2 = example_package.bump_color_rotate:main','pub_example = example_package.pub_example:main'
         ],
     },
 )
