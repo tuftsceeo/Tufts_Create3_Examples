5db4df40e9f12a27bf58d4ed83ff565207b8e2c0
kwujciak <60245413+kwujciak@users.noreply.github.com>
2022-07-07 15:02:42 -0400

    Update drive_square.py

--- example_package/example_package/drive_square.py
+++ example_package/example_package/drive_square.py
@@ -6,83 +6,134 @@ import time
 from irobot_create_msgs.action import DriveDistance
 from irobot_create_msgs.action import RotateAngle
 
-'''
-update comments
-'''
+
 
 class DriveDistanceActionClient(Node):
-# drive a specific distance
+    '''
+    This is an action client. Action clients send goal requests to action servers,
+    which sends goal feedback and results back to action clients. This action client
+    tells the robot to go forward 0.5m at a speed of 0.15. We are defining a class
+    "DriveDistanceActionClient" which is a subclass of Node. 
+    '''
 
     def __init__(self):
-# initialize node
+        '''
+        We initialize the class by calling the Node constructor then
+        naming our node 'drive_distance_action_client'
+        '''
+        
         super().__init__('drive_distance_action_client')
+        
+        '''
+        Here we initiate a new action server. We include where to add the action client
+        (self), the type of action (DriveDistance), and the action name ('drive_distance').
+        '''
+        
         self._action_client = ActionClient(
-            self, DriveDistance, '{Namespace}/drive_distance')
+            self, DriveDistance, 'Ygritte/drive_distance')
 
     def send_goal(self, distance=0.5, max_translation_speed=0.15):
+        '''
+        1. This method waits for the action server to be available.
+        '''
         goal_msg = DriveDistance.Goal()
         goal_msg.distance = distance
         goal_msg.max_translation_speed = max_translation_speed
 
         self._action_client.wait_for_server()
-
+        '''
+        2. Sends a goal to the server.
+        '''
         self._send_goal_future = self._action_client.send_goal_async(goal_msg)
-
+        '''
+        3. Returns a future to a goal handle. We need to register a callback 
+        for when the future is complete.
+        '''
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
     def goal_response_callback(self, future):
+        '''
+        A callback that is executed when the future is complete.
+        The future is completed when an action server accepts or rejects the goal request.
+        Since there will be no result, we can check and determine if the goal was rejected
+        and return early. 
+        '''
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected :(')
             return
 
         self.get_logger().info('Goal accepted :)')
+        
+        '''
+        We can request to see if the goal request was accepted or rejected.
+        Future will complete when the result is ready.
+        This step is registering a callback (similar to that of the goal response).
+        '''
 
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
-        
-        '''
-        Kate add
-        '''
-        
+
 
     def get_result_callback(self, future):
         '''
-        Future that will complete when the result is ready.
+        Here, we are logging the result sequence.
         '''
         result = future.result().result
         self.get_logger().info('Result: {0}'.format(result))
+        
+        '''
+        This shuts down the node.
+        '''
 
         rclpy.shutdown()
         
 
 class RotateActionClient(Node):
     '''
-    rotate 90º
+    This is an action client. Action clients send goal requests to action servers,
+    which sends goal feedback and results back to action clients. This action client
+    tells the robot to turn 90 degrees at a speed of 0.15. Subclass of Node.
     '''
 
     def __init__(self):
         '''
-initizalize node
+        We initialize the class by calling the Node constructor then
+        naming our node 'drive_distance_action_client'
         '''
         super().__init__('rotate_action_client')
+        '''
+        Here we initiate a new action server. We include where to add the action client
+        (self), the type of action (DriveDistance), and the action name ('drive_distance').
+        '''
+                
         self._action_client = ActionClient(self, RotateAngle, 'Ygritte/rotate_angle')
 
     def send_goal(self, angle=1.57, max_rotation_speed=0.5):
         '''
-        sends a goal to the server
+        1. This method waits for the action server to be available.
         '''
         goal_msg = RotateAngle.Goal()
         goal_msg.angle = angle 
         goal_msg.max_rotation_speed = max_rotation_speed
 
         self._action_client.wait_for_server()
+        '''
+        2. Sends a goal to the server.
+        '''        
         self._send_goal_future = self._action_client.send_goal_async(goal_msg)
+        '''
+        3. Returns a future to a goal handle. We need to register a callback 
+        for when the future is complete.
+        '''        
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
     def goal_response_callback(self, future):
-       '''
-       Callback that is executed when an action server accepts or rejects the goal request.
+        '''
+        A callback that is executed when the future is complete.
+        The future is completed when an action server accepts or rejects the goal request.
+        Since there will be no result, we can check and determine if the goal was rejected
+        and return early. 
         '''
         goal_handle = future.result()
         if not goal_handle.accepted:
@@ -90,43 +141,65 @@ initizalize node
             return
 
         self.get_logger().info('Goal accepted :)')
-
+        '''
+        We can request to see if the goal request was accepted or rejected.
+        Future will complete when the result is ready.
+        This step is registering a callback (similar to that of the goal response).
+        '''
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
 
     def get_result_callback(self, future):
         '''
-kate add
+        Here, we are logging the result sequence.
         '''
         result = future.result().result
         self.get_logger().info('Result: {0}'.format(result))
+        '''
+        This shuts down the node.
+        '''        
         rclpy.shutdown()
         
 def forward(args=None):
-
     dist  = 0.5
     speed = 0.25
-
+    '''
+    Initializes ROS2 and creates an instance of 
+    'DriveDistanceActionClient'
+    '''
     rclpy.init(args=args)
-    action_client1 = DriveDistanceActionClient()
+    action_client1 = DriveDistanceActionClient() 
+    '''
+    Sends a goal and waits until goal is done.
+    '''
     action_client1.send_goal(dist, speed)
     rclpy.spin(action_client1)
     time.sleep(0.5)
     
 def turn(args=None):
-
     angle = 1.57
     speed = 0.5
-
+   
+    '''
+    Initializes ROS2 and creates an instance of 
+    'RotateActionClient'
+    '''
     rclpy.init(args=args)
     action_client2 = RotateActionClient()
+    '''
+    Sends a goal and waits until goal is done.
+    '''
     action_client2.send_goal(angle, speed)
     rclpy.spin(action_client2)
     time.sleep(0.5)   
 
 
 def main(args=None):
-
+    '''
+    This for loop ensures that it goes forward and
+    turns 4 times to make a square. It is calling the forward
+    and turn functions to do so.
+    '''
     for i in range(4):
         forward()
         turn()
