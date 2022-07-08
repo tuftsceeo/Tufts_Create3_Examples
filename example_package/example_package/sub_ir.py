41ae4bac6f73d10756f6966575c67ac9302036ac
kwujciak <60245413+kwujciak@users.noreply.github.com>
2022-07-08 09:38:22 -0400

    Update sub_ir.py

--- example_package/example_package/sub_ir.py
+++ example_package/example_package/sub_ir.py
@@ -1,60 +1,86 @@
+'''
+sub_ir.py
+Tufts Create®3 example
+by Kate Wujciak 
+
+This file shows how to subscribe to a topic in ROS2 using the Create®3. It subscribes
+to the IR sensor and displays the relevant information in your terminal. 
+'''
+
+'''
+These statements import messages and topics. 
+'''
 import sys
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
-#from irobot_create_msgs.msg import HazardDetectionVector
 from irobot_create_msgs.msg import IrIntensityVector
-# from sensor_msgs.msg import BatteryState
 
 
 class IRSubscriber(Node):
     '''
-    An example of subscribing to a ROS2 topic.
-    A Node listening to the /ir_intensity topic.
+    The IRSubscriber class is created which is a subclass of Node.
+    The Node is subscribing to the /ir_intensity topic.
     '''
-    def __init__(self, namespace: str = "/JonSnow"):
+    
+    def __init__(self, namespace: str = "/{Namespace}"):
         '''
-        Purpose
-        -------
-        initialized by calling the Node constructor, naming our node 
-        'IR_subscriber'
+        The following line calls the Node class' constructor and declares a node name,
+        which is 'IR_subscriber' in this case. 
         '''
         super().__init__('IR_subscriber')
+        '''
+        This line indicates that the node is subscribing to the IrIntensityVector
+        type over the '/ir_intensity' topic. 
+        '''
         self.subscription = self.create_subscription(
             IrIntensityVector, namespace + '/ir_intensity', self.listener_callback,
             qos_profile_sensor_data)
 
     def listener_callback(self, msg:IrIntensityVector):
         '''
-        Purpose
-        -------
-        Whenever our subscriber (listener) gets a message this function is 
-        'called back' to and ran.
+        The subscriber's callback listens and as soon as it receives the message,
+        this function runs. 
+        This callback function is basically printing what it hears. It runs the data
+        it receives in your terminal (msg).  
         '''
-        #self.get_logger().info('I heard: "%s"' % msg)
         self.printIR(msg)
 
     def printIR(self, msg):
         '''
+        This function is used in the above function. Its purpose is to determine 
+        which parts of the info are worth showing.
         :type msg: IrIntensity
         :rtype: None
-        An example of how to get components of the msg returned from a topic.
+        The msg is returned from our topic '/ir_intensity.'
+        To get components of a message, use the '.' dot operator. 
         '''
-        # We can get components of the message by using the '.' dot operator
         for reading in msg.readings: 
         	val = reading.value
         	print("IR Sensor:", val)
 
 
 def main(args=None):
+    '''
+    This line initializes the rclpy library. 
+    '''
     rclpy.init(args=args)
-
+    '''
+    This creates the node.
+    '''
     IR_subscriber = IRSubscriber()
+    '''
+    The node is then "spun" so its callbacks are called.
+    '''
     try:
         rclpy.spin(IR_subscriber)
     except KeyboardInterrupt:
         print('\nCaught keyboard interrupt')
     finally:
+    '''
+    Destroying the node acts as a "reset" so we don't run into 
+    any problems when we try and run again.
+    '''
         print("Done")
         IR_subscriber.destroy_node()
         rclpy.shutdown()
