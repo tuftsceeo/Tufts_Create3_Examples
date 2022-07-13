8d1e1bb962bf0bb4126c16f680788c007c818814
kwujciak <60245413+kwujciak@users.noreply.github.com>
2022-07-13 13:30:16 -0400

    Update instrument.py

--- python_playground/instrument.py
+++ python_playground/instrument.py
@@ -4,6 +4,12 @@ from irobot_edu_sdk.music import Note
 
 robot = Create3(Bluetooth())
 duration = 0.15
+th = 150
+
+def front_obstacle(sensors):
+    print(sensors[3])
+    print(sensors[2])
+    return sensors[3], sensors[1] > th
 
 @event(robot.when_touched, [False, True])  # (..) button.
 async def touched(robot):
@@ -22,5 +28,6 @@ async def play(robot):
     while True:
         sensors = (await robot.get_ir_proximity()).sensors
         await robot.play_note(sensors[3], Note.QUARTER)  # sensors[3] is the central front sensor.
+        await robot.play_note(sensors[1], Note.QUARTER)
 
 robot.play()
