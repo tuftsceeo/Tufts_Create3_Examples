4d461a5e43d02d6c0f755810bc2417a8f476c1ba
kwujciak <60245413+kwujciak@users.noreply.github.com>
2022-07-13 10:28:45 -0400

    Update max_obstacles.py

--- python_playground/max_obstacles.py
+++ python_playground/max_obstacles.py
@@ -1,3 +1,6 @@
+'''
+To be commented
+'''
 from irobot_edu_sdk.backend.bluetooth import Bluetooth
 from irobot_edu_sdk.robots import event, hand_over, Color, Robot, Root, Create3
 from irobot_edu_sdk.music import Note
