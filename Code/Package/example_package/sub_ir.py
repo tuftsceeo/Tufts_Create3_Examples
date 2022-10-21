56ccd7ced3f20212f95461f81a7288135edfa792
kwujciak <60245413+kwujciak@users.noreply.github.com>
2022-10-20 20:55:05 -0400

    Update sub_ir.py

--- Code/Package/example_package/sub_ir.py
+++ Code/Package/example_package/sub_ir.py
@@ -5,47 +5,27 @@ by Kate Wujciak
 
 This file shows how to subscribe to a topic in ROS2 using the Create®3. It subscribes
 to the IR sensor and displays the relevant information in your terminal. 
-
-Today we are going to show you how to write a code that lets us subscribe to the 7 IR sensors 
-on the robot. IR stands for infrared. These sensors are used to detect objects. It could help 
-possibly avoid obstacles or know how far it is from a wall.
-'''
-
-'''
-We're going to write statements that import messages, actions, interfaces, and variable types.
-Import the rclpy module to gain access to the ros python client library
-The rclpy module is our holy grail for this course.
 '''
 
 import sys
 import rclpy
 
 '''
-We're going to be using sensor data from the IR vectors so we need to import
+Statements that import messages, actions, interfaces, and variable types.
 '''
 from rclpy.node import Node
-'''
-from rclpy.qos (qos stands for quality of service) so that the node knows what type of info we will be using 
-'''
 from rclpy.qos import qos_profile_sensor_data
-'''
-and then... we find this by going to the irobot create messages github 
-you can find other sensor messages such as battery state that are standard ros messages
-like battery BatteryState for example
-
-'''
 from irobot_create_msgs.msg import IrIntensityVector
 
 '''
-Input your namespace here as a global variable. Helps if you are using multiple robots.
+Input your namespace here as a global variable. 
 '''
 namespace = '[Namespace]'
 
 class IRSubscriber(Node):
     '''
     The IRSubscriber class is created which is a subclass of Node.
-    The Node is subscribing to the /ir_intensity topic. Classes help us organize code so 
-    we can bundle it together based on functionality.
+    The Node is subscribing to the /ir_intensity topic.
     '''
     
     def __init__(self):
@@ -55,72 +35,53 @@ class IRSubscriber(Node):
         '''
         super().__init__('IR_subscriber')
         '''
-        This following line indicates that the node is subscribing to the IrIntensityVector
-        type over the '/ir_intensity' topic. Now we are creating a subscription to this topic.
+        This line indicates that the node is subscribing to the IrIntensityVector
+        type over the '/ir_intensity' topic. 
         '''
         print('Creating subscription to to the IrIntensityVector type over the /ir_intensity topic')
-        '''
-        In these parentheses we have our IrIntensityVector type, the namespace and the topic its subscribing too, 
-        when we want to subscribe to the message so on a listener callback which we will go over later, 
-        and the type of quality of service data it’s receiving.
-        '''
         self.subscription = self.create_subscription(
             IrIntensityVector, namespace + '/ir_intensity', self.listener_callback,
             qos_profile_sensor_data)
-  
+
+    def listener_callback(self, msg:IrIntensityVector):
+        '''
+        The subscriber's callback listens and as soon as it receives the message,
+        this function runs. 
+        This callback function is basically printing what it hears. It runs the data
+        it receives in your terminal (msg).  
+        '''
+        print('Now listening to IR sensor readings it hears...')
+
+        self.printIR(msg)
+
     def printIR(self, msg):
         '''
-        Its purpose is to determine 
+        This function is used in the above function. Its purpose is to determine 
         which parts of the info are worth showing.
-        We want to print the IR sensor values that are important to us. 
-        We know what the IrIntensityVector msg spits out based on what irobot tells us
-        in their github as mentioned earlier. 
-        
         :type msg: IrIntensity
         :rtype: None
         The msg is returned from our topic '/ir_intensity.'
         To get components of a message, use the '.' dot operator. 
         '''
         print('Printing IR sensor readings:')
-        
-        '''
-        So we're gonna loop through every reading in our list of readings from the msg.
-        There is a header and a value within each reading
-        So we'll get the value of that reading again by using the dot operator and print it 
-
-        '''
         for reading in msg.readings: 
         	val = reading.value
-        	print("IR Sensor: " + str(val))      
-        
-    def listener_callback(self, msg:IrIntensityVector):
-        '''
-        Earlier, in the initialized function, we told it to run this function when it recieves a message.
-        The subscriber's callback listens and as soon as it receives the message,
-        this function runs. 
-        This callback function is basically printing what it hears, with the help of the function above. 
-        '''
-        print('Now listening to IR sensor readings it hears...')
-
-        self.printIR(msg)
+        	print("IR Sensor: " + str(val))
 
 
 def main(args=None):
     '''
-    This main function is outside of the class.
-    This line initializes the rclpy library, which is necessary in every main function that you write. 
+    This line initializes the rclpy library. 
     '''
     rclpy.init(args=args)
-
+    '''
+    This creates the node.
+    '''
     IR_subscriber = IRSubscriber()
     '''
-    Now we'll create the node we had earlier defined with our IRSubscriber(). 
-    The node is then "spun" (start subscribing to the topic) so its callbacks are called.
+    The node is then "spun" so its callbacks are called.
     '''
     print('Callbacks are called.')
-    '''
-    this keyboard interupt is how we'll tell it to stop recieving data from the robot. 
-    '''
     try:
         rclpy.spin(IR_subscriber)
     except KeyboardInterrupt:
@@ -133,9 +94,7 @@ def main(args=None):
     	print("Done")
     	IR_subscriber.destroy_node()
     	rclpy.shutdown()
-'''
-navigate to the directory that this file is in. Then simply write python3 sub_ir.py, which is an easy way to run code, and unique to python. 
-'''
+
 
 if __name__ == '__main__':
     main()
